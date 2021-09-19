62055dded9ad6231879f5f114d22cec10408820c
SimranJhhanji <55383707+SimranJhhanji@users.noreply.github.com>
2021-09-19 17:01:13 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp2
+{
+    class Employee
+    {
+        private string name;
+        private short age;
+        private string emptype;
+        
+        
+
+        public Employee(string name, short age, string emptype)
+        {
+            this.name = name;
+            this.age = age;
+            this.emptype = emptype;
+            
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public short Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Emp_Type
+        {
+            get { return emptype; }
+            set { emptype = value; }
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> elist = new List<Employee>();
+            //creating new Employee object 
+            elist.Add(new Employee("Mahesh Chand", 35,"Temp"));
+            
+
+            foreach (var author in elist)
+            {
+                Console.WriteLine("Name = {0}, Age = {1}, Type of Employee = {2}", author.Name ,author.Age, author.Emp_Type);
+            }
+
+            try
+            {
+
+                StreamWriter sw = new StreamWriter(@"F:\data2.txt");
+
+                sw.Write(elist);
+
+
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("***The file is generated at data2.txt***");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+    }
+
